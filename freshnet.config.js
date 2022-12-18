02713cbca470b0d349864a2efa480804f95bbd64
JimmyChane <63626658+JimmyChane@users.noreply.github.com>
2022-12-18 14:29:03 +0800

    Update freshnet.config.js

--- freshnet.config.js
+++ freshnet.config.js
@@ -1,13 +1,13 @@
 module.exports = {
 	devPort: 8080,
 
-	host: "http://127.0.0.1",
-	hostApi: "http://127.0.0.1/api",
-	hostRes: "http://127.0.0.1/resource",
+	// host: "http://127.0.0.1",
+	// hostApi: "http://127.0.0.1/api",
+	// hostRes: "http://127.0.0.1/resource",
 
-	// host: "https://www.freshnet.app",
-	// hostApi: "https://www.freshnet.app/api",
-	// hostRes: "https://www.freshnet.app/resource",
+	host: "https://www.freshnet.app",
+	hostApi: "https://www.freshnet.app/api",
+	hostRes: "https://www.freshnet.app/resource",
 
 	cloudinaryRes: "https://res.cloudinary.com/freshnet/image/upload/resource",
 
