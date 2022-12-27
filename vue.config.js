d58d71a340237878dc7870b0dfe8c6e73dbf69cf
JimmyChane <63626658+JimmyChane@users.noreply.github.com>
2022-12-27 16:59:09 +0800

    optimize

--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,13 @@
 const TimeBuilder = require("./src/tools/TimeBuilder.js");
 const timestamp = TimeBuilder.getCurrent("-");
-const config = require("./freshnet.config.js");
+const freshnetConfig = require("./freshnet.config.js");
 
-module.exports = {
+const vueConfig = {
 	publicPath: "./",
-	outputDir: config.exportAbsolutePath,
 	productionSourceMap: false,
 	devServer: {
 		disableHostCheck: true,
-		port: config.devPort,
+		port: freshnetConfig.devPort,
 		public: "0.0.0.0:8080",
 		// open: process.platform === "darwin",
 		host: "0.0.0.0",
@@ -31,49 +30,10 @@ module.exports = {
 			return args;
 		});
 	},
-	/* pwa: {
-		manifestOptions: {
-			name: "Freshnet Enterprise",
-			short_name: "Freshnet",
-			icons: [
-				{ src: "./logos-192.png", type: "image/png", sizes: "192x192" },
-				{ src: "./logos-512.png", type: "image/png", sizes: "512x512" },
-				{ src: "./logos-512.svg", type: "image/svg+xml", sizes: "512x512" },
-			],
-			start_url: "/",
-			background_color: "#f0f0f0",
-			display: "standalone",
-			scope: "/",
-			theme_color: "#2f579d",
-		},
-
-		workboxPluginMode: "InjectManifest",
-		workboxOptions: { swSrc: "src/sw.js" },
-
-		msTileColor: "#000000",
-		appleMobileWebAppCapable: "no",
-		appleMobileWebAppStatusBarStyle: "default",
-
-		assetsVersion: "",
-		manifestPath: "manifest.json",
-
-		manifestCrossorigin: undefined,
-
-		// workboxOptions: {
-		//    navigateFallback: "./index.html",
-		//    runtimeCaching: [
-		//       {
-		//          urlPattern: new RegExp("^https://api.zippopoptam.us/us/"),
-		//          handler: "networkFirst",
-		//          options: {
-		//             networkTimeoutSeconds: 20,
-		//             cacheName: "api-cache",
-		//             cacheableResponse: {
-		//                statuses: [0, 200],
-		//             },
-		//          },
-		//       },
-		//    ],
-		// },
-	}, */
 };
+
+if (typeof freshnetConfig.exportAbsolutePath === "string") {
+	vueConfig["outputDir"] = freshnetConfig.exportAbsolutePath;
+}
+
+module.exports = vueConfig;
---
+++ vue.config.old.js
@@ -0,0 +1,47 @@
+const vueConfig = {
+	/* pwa: {
+		manifestOptions: {
+			name: "Freshnet Enterprise",
+			short_name: "Freshnet",
+			icons: [
+				{ src: "./logos-192.png", type: "image/png", sizes: "192x192" },
+				{ src: "./logos-512.png", type: "image/png", sizes: "512x512" },
+				{ src: "./logos-512.svg", type: "image/svg+xml", sizes: "512x512" },
+			],
+			start_url: "/",
+			background_color: "#f0f0f0",
+			display: "standalone",
+			scope: "/",
+			theme_color: "#2f579d",
+		},
+
+		workboxPluginMode: "InjectManifest",
+		workboxOptions: { swSrc: "src/sw.js" },
+
+		msTileColor: "#000000",
+		appleMobileWebAppCapable: "no",
+		appleMobileWebAppStatusBarStyle: "default",
+
+		assetsVersion: "",
+		manifestPath: "manifest.json",
+
+		manifestCrossorigin: undefined,
+
+		// workboxOptions: {
+		//    navigateFallback: "./index.html",
+		//    runtimeCaching: [
+		//       {
+		//          urlPattern: new RegExp("^https://api.zippopoptam.us/us/"),
+		//          handler: "networkFirst",
+		//          options: {
+		//             networkTimeoutSeconds: 20,
+		//             cacheName: "api-cache",
+		//             cacheableResponse: {
+		//                statuses: [0, 200],
+		//             },
+		//          },
+		//       },
+		//    ],
+		// },
+	}, */
+};
