28858f8e8772b13a6146b99e295fa56069f4d465
JimmyChane <63626658+JimmyChane@users.noreply.github.com>
2023-01-01 01:12:59 +0800

    glitch fix
    - fix for price where "," could be place after "-"

--- freshnet.config.js
+++ freshnet.config.js
@@ -1,14 +1,14 @@
-// const host = "http://127.0.0.1";
-const host = "https://www.freshnet.app";
+const host = "http://127.0.0.1";
+// const host = "https://www.freshnet.app";
 
 module.exports = {
-	devPort: 8080,
+  devPort: 8080,
 
-	host,
-	hostApi: `${host}/api`,
-	hostRes: `${host}/resource`,
+  host,
+  hostApi: `${host}/api`,
+  hostRes: `${host}/resource`,
 
-	cloudinaryRes: "https://res.cloudinary.com/freshnet/image/upload/resource",
+  cloudinaryRes: "https://res.cloudinary.com/freshnet/image/upload/resource",
 
-	exportAbsolutePath: "../freshnet-express/public/page",
+  exportAbsolutePath: "../freshnet-express/public/page",
 };
--- src/objects/Price.js
+++ src/objects/Price.js
@@ -1,119 +1,121 @@
 export default class Price {
-	static #parseCurrency(content) {
-		const indexStart = content.indexOf(this.DefaultCurrency);
-		const indexEnd = indexStart === -1 ? -1 : indexStart + this.DefaultCurrency.length;
-		return {
-			indexStart,
-			indexEnd,
-			value: indexStart === -1 ? "" : content.substring(indexStart, indexEnd),
-		};
-	}
-	static #parseAmount(content) {
-		let valueString = "";
-		let hasNumberFront = false;
-		let hasNumberBack = false;
-		let hasDot = false;
-		for (let i = 0; i < content.length; i++) {
-			let char = content.charAt(i);
+  static #parseCurrency(content) {
+    const indexStart = content.indexOf(this.DefaultCurrency);
+    const indexEnd =
+      indexStart === -1 ? -1 : indexStart + this.DefaultCurrency.length;
+    return {
+      indexStart,
+      indexEnd,
+      value: indexStart === -1 ? "" : content.substring(indexStart, indexEnd),
+    };
+  }
+  static #parseAmount(content) {
+    let valueString = "";
+    let hasNumberFront = false;
+    let hasNumberBack = false;
+    let hasDot = false;
+    for (let i = 0; i < content.length; i++) {
+      let char = content.charAt(i);
 
-			if (char === " ") {
-				if (hasDot || hasNumberFront || hasNumberBack) break;
-				continue;
-			}
+      if (char === " ") {
+        if (hasDot || hasNumberFront || hasNumberBack) break;
+        continue;
+      }
 
-			let parse = Number.parseInt(char);
-			if (!Number.isNaN(parse)) {
-				valueString += parse;
-				if (hasDot) hasNumberBack = true;
-				else hasNumberFront = true;
-				continue;
-			}
+      let parse = Number.parseInt(char);
+      if (!Number.isNaN(parse)) {
+        valueString += parse;
+        if (hasDot) hasNumberBack = true;
+        else hasNumberFront = true;
+        continue;
+      }
 
-			if (char === ".") {
-				if (hasDot) break;
-				valueString += ".";
-				hasDot = true;
-				continue;
-			}
-		}
+      if (char === ".") {
+        if (hasDot) break;
+        valueString += ".";
+        hasDot = true;
+        continue;
+      }
+    }
 
-		const valueParse = Number.parseFloat(valueString);
+    const valueParse = Number.parseFloat(valueString);
 
-		return Number.isNaN(valueParse) ? 0 : valueParse;
-	}
+    return Number.isNaN(valueParse) ? 0 : valueParse;
+  }
 
-	static get DefaultCurrency() {
-		return "RM";
-	}
-	static parse(any) {
-		if (any instanceof Price) return any;
-		if (typeof any === "number") return new Price(any);
-		if (typeof any === "string") {
-			const currencyParses = [];
-			const amountParsed = this.#parseAmount(any);
+  static get DefaultCurrency() {
+    return "RM";
+  }
+  static parse(any) {
+    if (any instanceof Price) return any;
+    if (typeof any === "number") return new Price(any);
+    if (typeof any === "string") {
+      const currencyParses = [];
+      const amountParsed = this.#parseAmount(any);
 
-			while (true) {
-				let currencyParsed = this.#parseCurrency(any);
-				if (!currencyParsed.value) break;
-				currencyParses.push(currencyParsed);
-				any = any.substring(currencyParsed.indexEnd, any.length);
-			}
+      while (true) {
+        let currencyParsed = this.#parseCurrency(any);
+        if (!currencyParsed.value) break;
+        currencyParses.push(currencyParsed);
+        any = any.substring(currencyParsed.indexEnd, any.length);
+      }
 
-			return new Price(
-				amountParsed,
-				currencyParses.length ? currencyParses[0].value : "",
-			);
-		}
+      return new Price(
+        amountParsed,
+        currencyParses.length ? currencyParses[0].value : "",
+      );
+    }
 
-		return new Price();
-	}
+    return new Price();
+  }
 
-	#amount;
-	#currency;
+  #amount;
+  #currency;
 
-	constructor(amount = 0, currency = "RM") {
-		this.#currency =
-			typeof currency === "string"
-				? currency.trim().replace(" ", "").toUpperCase()
-				: "";
-		this.#currency =
-			this.#currency.length === 0 ? this.DefaultCurrency : this.#currency;
-		this.#amount = isNaN(amount) ? 0 : Number(amount);
-	}
+  constructor(amount = 0, currency = "RM") {
+    this.#currency =
+      typeof currency === "string"
+        ? currency.trim().replace(" ", "").toUpperCase()
+        : "";
+    this.#currency =
+      this.#currency.length === 0 ? this.DefaultCurrency : this.#currency;
+    this.#amount = isNaN(amount) ? 0 : Number(amount);
+  }
 
-	get amount() {
-		return this.#amount;
-	}
-	get currency() {
-		return this.#currency;
-	}
+  get amount() {
+    return this.#amount;
+  }
+  get currency() {
+    return this.#currency;
+  }
 
-	compare() {
-		return this.#amount - item.#amount;
-	}
+  compare() {
+    return this.#amount - item.#amount;
+  }
 
-	plus(any) {
-		const price = Price.parse(any);
-		return new Price(this.#amount + price.#amount, this.#currency);
-	}
-	minus(any) {
-		const price = Price.parse(any);
-		return new Price(this.#amount - price.#amount, this.#currency);
-	}
+  plus(any) {
+    const price = Price.parse(any);
+    return new Price(this.#amount + price.#amount, this.#currency);
+  }
+  minus(any) {
+    const price = Price.parse(any);
+    return new Price(this.#amount - price.#amount, this.#currency);
+  }
 
-	toString() {
-		const { amount, currency } = this;
+  toString() {
+    const { amount, currency } = this;
 
-		let text = amount.toFixed(2);
-		let comaIndex = text.indexOf(".") - 3;
-		while (comaIndex > 0) {
-			text = `${text.substring(0, comaIndex)},${text.substring(
-				comaIndex,
-				text.length,
-			)}`;
-			comaIndex = comaIndex - 3;
-		}
+    let text = amount.toFixed(2);
+    let dotIndex = text.indexOf(".") - 3;
+    let minusIndex = text.indexOf("-") + 1;
+    while (dotIndex > minusIndex) {
+      const part1 = `${text.substring(0, dotIndex)}`;
+      const part2 = `${text.substring(dotIndex, text.length)}`;
 
-		return currency ? `${currency} ${text}` : `${text}`;
-	}
+      text = `${part1},${part2}`;
+      dotIndex = dotIndex - 3;
+    }
+
+    return currency ? `${currency} ${text}` : `${text}`;
+  }
 }
