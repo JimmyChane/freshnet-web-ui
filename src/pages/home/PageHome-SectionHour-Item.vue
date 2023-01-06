e32c450d2d14057b81d2d35f58b0548ab6908a0d
JimmyChane <63626658+JimmyChane@users.noreply.github.com>
2023-01-06 14:06:36 +0800

    functionality
    - detecting and suggesting hours

--- src/pages/home/PageHome-SectionHour-Item.vue
+++ src/pages/home/PageHome-SectionHour-Item.vue
@@ -1,47 +1,57 @@
 <script>
-	export default {
-		props: {
-			item: { type: Object },
-			isToday: { type: Boolean, default: false },
-		},
-	};
+   export default {
+      props: {
+         item: { type: Object },
+      },
+      computed: {
+         isToday() {
+            return this.item.isSameDay(new Date());
+         },
+         title() {
+            return this.item.title;
+         },
+         content() {
+            return this.item.hours.toString();
+         },
+      },
+   };
 </script>
 
 <template>
-	<div :class="['HourItem', isToday ? 'HourItem-isToday' : '']">
-		<span class="HourItem-title">{{ item.title }}</span>
-		<span class="HourItem-content">{{ item.content }}</span>
-	</div>
+   <div :class="['HourItem', isToday ? 'HourItem-isToday' : '']">
+      <span class="HourItem-title">{{ title }}</span>
+      <span class="HourItem-content">{{ content }}</span>
+   </div>
 </template>
 
 <style lang="scss" scoped>
-	.HourItem {
-		color: hsla(0, 0%, 0%, 0.8);
-		font-weight: 400;
+   .HourItem {
+      color: hsla(0, 0%, 0%, 0.8);
+      font-weight: 400;
 
-		width: 100%;
+      width: 100%;
 
-		display: flex;
-		flex-direction: row;
-		gap: 0.5em;
-		padding: 0 0.5em;
+      display: flex;
+      flex-direction: row;
+      gap: 0.5em;
+      padding: 0 0.5em;
 
-		.HourItem-title {
-			text-align: start;
-			width: 6em;
-		}
-		.HourItem-content {
-			flex-grow: 1;
-			text-align: end;
-		}
-	}
-	.HourItem-isToday {
-		font-weight: 900;
-		color: black;
-		background-color: rgba(0, 0, 0, 0.03);
-		background-color: black;
-		color: white;
-		padding: 0.3em 0.5em;
-		border-radius: 0.5em;
-	}
+      .HourItem-title {
+         text-align: start;
+         width: 6em;
+      }
+      .HourItem-content {
+         flex-grow: 1;
+         text-align: end;
+      }
+   }
+   .HourItem-isToday {
+      font-weight: 900;
+      color: black;
+      background-color: rgba(0, 0, 0, 0.03);
+      background-color: black;
+      color: white;
+      padding: 0.3em 0.5em;
+      border-radius: 0.5em;
+   }
 </style>
--- src/pages/home/PageHome.vue
+++ src/pages/home/PageHome.vue
@@ -1,199 +1,263 @@
 <script>
-	import SearchInput from "@/components/SearchInput.vue";
-	import Footer from "@/app/footer/Footer.vue";
+   import SearchInput from "@/components/SearchInput.vue";
+   import Footer from "@/app/footer/Footer.vue";
 
-	import Actionbar from "./PageHome-Actionbar.vue";
-	import Header from "./PageHome-Header.vue";
+   import Actionbar from "./PageHome-Actionbar.vue";
+   import Header from "./PageHome-Header.vue";
 
-	import SectionProduct from "./PageHome-SectionProduct.vue";
-	import SectionContact from "./PageHome-SectionContact.vue";
-	import SectionPrint from "./PageHome-SectionPrint.vue";
-	import SectionLocation from "./PageHome-SectionLocation.vue";
-	import SectionHour from "./PageHome-SectionHour.vue";
-	import SectionWhatElse from "./PageHome-SectionWhatElse.vue";
-	import SectionAboutUs from "./PageHome-SectionAboutUs.vue";
-	import SectionFeedback from "./PageHome-SectionFeedback.vue";
+   import SectionProduct from "./PageHome-SectionProduct.vue";
+   import SectionContact from "./PageHome-SectionContact.vue";
+   import SectionPrint from "./PageHome-SectionPrint.vue";
+   import SectionLocation from "./PageHome-SectionLocation.vue";
+   import SectionHour from "./PageHome-SectionHour.vue";
+   import SectionWhatElse from "./PageHome-SectionWhatElse.vue";
+   import SectionAboutUs from "./PageHome-SectionAboutUs.vue";
+   import SectionFeedback from "./PageHome-SectionFeedback.vue";
+   import Company from "@/host/Company";
+   import {
+      format,
+      differenceInHours,
+      differenceInMinutes,
+      parse,
+   } from "date-fns";
 
-	export default {
-		key: "home",
-		name: "Home",
-		title: "Home",
-		icon: { light: "home-FFFFFF", dark: "home-000000" },
+   export default {
+      key: "home",
+      name: "Home",
+      title: "Home",
+      icon: { light: "home-FFFFFF", dark: "home-000000" },
 
-		components: {
-			SearchInput,
-			Footer,
-			Actionbar,
-			Header,
-			SectionProduct,
-			SectionContact,
-			SectionPrint,
-			SectionLocation,
-			SectionHour,
-			SectionWhatElse,
-			SectionAboutUs,
-			SectionFeedback,
-		},
-		data() {
-			return { scrollTop: 0 };
-		},
-		computed: {
-			isThin: (c) => c.$root.navigation.isDrawer(),
+      components: {
+         SearchInput,
+         Footer,
+         Actionbar,
+         Header,
+         SectionProduct,
+         SectionContact,
+         SectionPrint,
+         SectionLocation,
+         SectionHour,
+         SectionWhatElse,
+         SectionAboutUs,
+         SectionFeedback,
+      },
+      data() {
+         return { scrollTop: 0 };
+      },
+      computed: {
+         isThin: (c) => c.$root.navigation.isDrawer(),
 
-			classes() {
-				if (this.$root.window.innerWidth > 1170) return "Home-isHorizontal";
-				return "Home-isVertical";
-			},
-		},
-		mounted() {
-			document.title = "Freshnet Enterprise";
-		},
-	};
+         classes() {
+            if (this.$root.window.innerWidth > 1170) return "Home-isHorizontal";
+            return "Home-isVertical";
+         },
+
+         businessHourDescription() {
+            const now = new Date();
+            // const now = parse("Monday 11:59pm", "cccc hh:mmaa", new Date());
+            const days = Company.BusinessDays.toArray();
+
+            const today = days.find((day) => day.isToday());
+            const todayHourEnd = today.hours.getDateEnd();
+
+            const remainingHourCount = differenceInMinutes(todayHourEnd, now);
+
+            if (0 < remainingHourCount && remainingHourCount <= 30)
+               return `Closing Soon until ${format(todayHourEnd, "h:mmaaa")}`;
+
+            if (today.hours.isBetween(now))
+               return `We're open until ${format(todayHourEnd, "h:mmaaa")}`;
+
+            if (today.isSameDay(now) && today.hours.isBefore(now)) {
+               const todayHourStart = today.hours.getDateStart();
+               return `Sorry, we're not open now\nCome back at ${format(
+                  todayHourStart,
+                  "h:mmaaa",
+               )}`;
+            }
+
+            const nextDay = Company.BusinessDays.getNextWorkingDay(today);
+            const nextDayStartDate = nextDay.hours.getDateStart();
+            console.log(format(nextDayStartDate, "cccc"));
+            return `Sorry, we're closed\nCome back at ${format(
+               nextDayStartDate,
+               "h:mmaaa",
+            )} tomorrow`;
+         },
+      },
+      mounted() {
+         document.title = "Freshnet Enterprise";
+      },
+   };
 </script>
 
 <template>
-	<div
-		:class="['PageHome', classes, scrollTop > 0 ? 'Home-isScrollUp' : '']"
-		@scroll="(e) => (scrollTop = e.target.scrollTop)"
-	>
-		<Actionbar class="Home-actionbar" :isThin="isThin" />
+   <div
+      :class="['PageHome', classes, scrollTop > 0 ? 'Home-isScrollUp' : '']"
+      @scroll="(e) => (scrollTop = e.target.scrollTop)"
+   >
+      <Actionbar class="Home-actionbar" :isThin="isThin" />
 
-		<div class="Home-body">
-			<div>
-				<Header class="Home-header" />
-			</div>
+      <div class="Home-body">
+         <div>
+            <Header class="Home-header" />
+         </div>
 
-			<div class="Home-section-1">
-				<SectionProduct
-					:style="{ 'grid-column': 'auto / span 2', 'grid-row': 'auto / span 4' }"
-					:isThin="isThin"
-				/>
-				<SectionPrint
-					:style="{ 'grid-column': 'auto / span 2', 'grid-row': 'auto / span 2' }"
-					:isThin="isThin"
-				/>
-				<SectionLocation
-					:style="{ 'grid-column': 'auto / span 2', 'grid-row': 'auto / span 2' }"
-					:isThin="isThin"
-				/>
-			</div>
+         <div class="Home-section-1">
+            <SectionProduct
+               :style="{
+                  'grid-column': 'auto / span 2',
+                  'grid-row': 'auto / span 4',
+               }"
+               :isThin="isThin"
+            />
+            <SectionPrint
+               :style="{
+                  'grid-column': 'auto / span 2',
+                  'grid-row': 'auto / span 2',
+               }"
+               :isThin="isThin"
+            />
+            <SectionLocation
+               :style="{
+                  'grid-column': 'auto / span 2',
+                  'grid-row': 'auto / span 2',
+               }"
+               :isThin="isThin"
+            />
+         </div>
 
-			<div>
-				<span class="Home-section-title">Contact Us</span>
-				<SectionContact :isThin="isThin" />
-			</div>
+         <div>
+            <span class="Home-section-title">Contact Us</span>
+            <SectionContact :isThin="isThin" />
+         </div>
 
-			<div>
-				<span class="Home-section-title">Business Hours</span>
-				<SectionHour :isThin="isThin" />
-			</div>
+         <div>
+            <span class="Home-section-title">Business Hours</span>
 
-			<div>
-				<span class="Home-section-title">What else can we do?</span>
-				<SectionWhatElse :isThin="isThin" />
-			</div>
-		</div>
+            <p class="Home-HourDescription" v-if="businessHourDescription">{{
+               businessHourDescription
+            }}</p>
 
-		<Footer />
-	</div>
+            <SectionHour :isThin="isThin" />
+         </div>
+
+         <div>
+            <span class="Home-section-title">What else can we do?</span>
+            <SectionWhatElse :isThin="isThin" />
+         </div>
+      </div>
+
+      <Footer />
+   </div>
 </template>
 
 <style lang="scss" scoped>
-	.PageHome {
-		z-index: 1;
-		width: 100%;
-		height: 100%;
+   .PageHome {
+      z-index: 1;
+      width: 100%;
+      height: 100%;
 
-		display: flex;
-		flex-grow: 1;
-		flex-direction: column;
-		align-items: center;
-		color: black;
+      display: flex;
+      flex-grow: 1;
+      flex-direction: column;
+      align-items: center;
+      color: black;
 
-		position: relative;
-		overflow-x: hidden;
-		overflow-y: auto;
+      position: relative;
+      overflow-x: hidden;
+      overflow-y: auto;
 
-		.Home-actionbar {
-			position: sticky;
-			top: 0;
-			z-index: 2;
-			border-bottom: 1px solid transparent;
-		}
-		.Home-body {
-			z-index: 1;
-			width: 100%;
-			gap: 1rem;
+      .Home-actionbar {
+         position: sticky;
+         top: 0;
+         z-index: 2;
+         border-bottom: 1px solid transparent;
+      }
+      .Home-body {
+         z-index: 1;
+         width: 100%;
+         gap: 1rem;
 
-			display: flex;
-			flex-direction: column;
-			align-items: stretch;
+         display: flex;
+         flex-direction: column;
+         align-items: stretch;
 
-			& > * {
-				width: 100%;
-				gap: 0.5rem;
-				display: flex;
-				flex-direction: column;
+         & > * {
+            width: 100%;
+            gap: 0.5rem;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            margin-top: 2rem;
 
-				.Home-section-title {
-					font-size: 1.4rem;
-					font-weight: 500;
+            .Home-section-title {
+               font-size: 1.4rem;
+               font-weight: 500;
 
-					font-size: 2rem;
-					color: hsl(0, 0%, 13%);
+               font-size: 2rem;
+               color: hsl(0, 0%, 13%);
 
-					padding-top: 3rem;
-					padding-bottom: 2rem;
+               margin-bottom: 1rem;
 
-					display: flex;
-					align-items: center;
-					justify-content: center;
-					text-align: center;
-				}
-			}
-		}
-	}
-	.Home-isScrollUp {
-		.Home-actionbar {
-			border-bottom: 1px solid #0000001a;
-		}
-	}
+               display: flex;
+               align-items: center;
+               justify-content: center;
+               text-align: center;
+            }
 
-	.Home-isVertical {
-		--actionbar-height: 6rem;
-		.Home-body {
-			padding: 1.2rem;
+            .Home-HourDescription {
+               margin-bottom: 1rem;
 
-			.Home-section-1 {
-				display: grid;
-				grid-template-columns: 1fr 1fr;
-				grid-auto-flow: row;
+               display: flex;
+               flex-direction: column;
+               align-items: center;
+               text-align: center;
+            }
+         }
+      }
+   }
+   .Home-isScrollUp {
+      .Home-actionbar {
+         border-bottom: 1px solid #0000001a;
+      }
+   }
 
-				justify-content: center;
-				align-items: center;
-				justify-items: center;
-				align-content: center;
-			}
-		}
-	}
-	.Home-isHorizontal {
-		--actionbar-height: 3.5rem;
-		.Home-body {
-			height: max-content;
-			max-width: 80rem;
-			max-width: 70rem;
-			padding: 2rem;
+   .Home-isVertical {
+      --actionbar-height: 6rem;
+      .Home-body {
+         padding: 1.2rem;
 
-			.Home-section-1 {
-				display: grid;
-				grid-template-columns: 1fr 1fr 1fr 1fr;
-				grid-auto-flow: row;
+         .Home-section-1 {
+            display: grid;
+            grid-template-columns: 1fr 1fr;
+            grid-auto-flow: row;
 
-				justify-content: center;
-				align-items: center;
-				justify-items: center;
-				align-content: center;
-			}
-		}
-	}
+            justify-content: center;
+            align-items: center;
+            justify-items: center;
+            align-content: center;
+         }
+      }
+   }
+   .Home-isHorizontal {
+      --actionbar-height: 3.5rem;
+      .Home-body {
+         height: max-content;
+         max-width: 80rem;
+         max-width: 70rem;
+         padding: 2rem;
+
+         .Home-section-1 {
+            display: grid;
+            grid-template-columns: 1fr 1fr 1fr 1fr;
+            grid-auto-flow: row;
+
+            justify-content: center;
+            align-items: center;
+            justify-items: center;
+            align-content: center;
+         }
+      }
+   }
 </style>
