5bfe9950afe40675e6f4e6f71e9dae01b1de6d95
JimmyChane <63626658+JimmyChane@users.noreply.github.com>
2023-01-30 12:30:33 +0800

    fix
    - fix for parsing port incorrectly

--- src/host/AppHost.js
+++ src/host/AppHost.js
@@ -1,19 +1,34 @@
-import ApiHost from "./ApiHost.js";
+import U from "@/U";
 
-let { protocol, hostname, port } = window.location;
+const getClientProtocol = () => window.location.protocol;
+const getClientHostname = () => window.location.hostname;
+const getClientPort = () => window.location.port;
+const parsePort = (port) => {
+   if (!U.isNumber(port)) port = Number.parseInt(port);
+   if (Number.isNaN(port)) port = 80;
+   return port;
+};
 
 class AppHost {
    static #dev_port = 8080;
    static #parseOrigin(protocol, hostname, port) {
-      port = port === 80 ? "" : `:${port}`;
-      return `${protocol}//${hostname}${port}`;
+      port = parsePort(port);
+      return port === 80
+         ? `${protocol}//${hostname}`
+         : `${protocol}//${hostname}:${port}`;
    }
 
    get origin() {
-      return AppHost.#parseOrigin(protocol, hostname, port);
+      return AppHost.#parseOrigin(
+         getClientProtocol(),
+         getClientHostname(),
+         getClientPort(),
+      );
    }
    get path() {
-      return port == AppHost.#dev_port ? `${this.origin}/#` : `${this.origin}/page/#`;
+      return getClientPort() == AppHost.#dev_port
+         ? `${this.origin}/#`
+         : `${this.origin}/page/#`;
    }
 }
 
