362b2ce01800a75a55a17b37a3863a1974de2e49
JimmyChane <63626658+JimmyChane@users.noreply.github.com>
2023-03-04 10:58:38 +0800

    Update service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,46 +1,46 @@
 const cacheName = "static-v1";
+const version = "";
 const assets = [
-	"./",
-	"./page/index.html",
-	"./page/manifest.webmanifest",
-	"./page/favicon.ico",
-	"./page/favicons-32.ico",
-	"./page/logo.png",
-	"./page/logos-129.png",
-	"./page/logos-192.png",
-	"./page/logos-512.png",
-	"./page/logos-512.svg",
-	// "./page/js/app.js",
-	// "./page/js/app.chunk.js",
-	// "./page/css/style.css",
-	// "./page/css/style.chunk.css",
+   "./",
+   "./page/index.html",
+   "./page/manifest.webmanifest",
+   "./page/favicon.ico",
+   "./page/favicons-32.ico",
+   "./page/logo.png",
+   "./page/logos-129.png",
+   "./page/logos-192.png",
+   "./page/logos-512.png",
+   "./page/logos-512.svg",
+   `./page/js/app.${version}.js`,
+   `./page/js/app.${version}.chunk.js`,
+   `./page/css/style.${version}.css`,
 ];
 
 self.addEventListener("install", async (event) => {
-	const promise = Promise.resolve().then(async () => {
-		const requests = assets.map((urlAsset) => new Request(urlAsset));
-		const cacheStore = await caches.open(cacheName);
-		cacheStore.addAll(requests);
-	});
-	event.waitUntil(promise);
+   const promise = Promise.resolve().then(async () => {
+      const requests = assets.map((urlAsset) => new Request(urlAsset));
+      const cacheStore = await caches.open(cacheName);
+      cacheStore.addAll(requests);
+   });
+   event.waitUntil(promise);
 });
 // self.addEventListener("activate", async (event) => {});
 self.addEventListener("fetch", async (event) => {
-	if (event.request.method !== "GET") return;
+   if (event.request.method !== "GET") return;
 
-	const promise = Promise.resolve()
-		.then(async () => {
-			const cacheStore = await caches.open(cacheName);
-			try {
-				const response = await fetch(event.request);
-				cacheStore.put(event.request, response.clone());
-				return response;
-			} catch (error) {
-				return await cacheStore.match(event.request);
-			}
-		})
-		.then((response) => {
-			if (response instanceof Response) return response;
-		});
-	event.respondWith(promise);
+   const promise = Promise.resolve()
+      .then(async () => {
+         const cacheStore = await caches.open(cacheName);
+         try {
+            const response = await fetch(event.request);
+            cacheStore.put(event.request, response.clone());
+            return response;
+         } catch (error) {
+            return await cacheStore.match(event.request);
+         }
+      })
+      .then((response) => {
+         if (response instanceof Response) return response;
+      });
+   event.respondWith(promise);
 });
