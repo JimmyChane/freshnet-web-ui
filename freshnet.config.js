edfb78cb1e7111fc4b111602cf4b62bafd0704a1
JimmyChane <63626658+JimmyChane@users.noreply.github.com>
2023-04-09 13:51:09 +0800

    Refactor config.js

--- freshnet.config.js
+++ freshnet.config.js
@@ -9,5 +9,5 @@ module.exports = {
 
    cloudinaryRes: "https://res.cloudinary.com/freshnet/image/upload/resource",
 
-   // exportAbsolutePath: "../freshnet-express/public/page",
+   exportAbsolutePath: "../freshnet-frontend/public/page",
 };
