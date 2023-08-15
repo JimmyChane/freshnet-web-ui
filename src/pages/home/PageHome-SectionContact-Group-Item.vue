64be4d2707f4aa3e7e4a5e21bc492c613ebb7deb
JimmyChane <63626658+JimmyChane@users.noreply.github.com>
2023-08-15 22:49:40 +0800

    Optimize Contacts

--- src/pages/home/PageHome-SectionContact-Group-Item.vue
+++ src/pages/home/PageHome-SectionContact-Group-Item.vue
@@ -1,38 +1,44 @@
 <script>
-   export default {
-      props: { item: { type: Object } },
-      computed: {
-         title: (c) => c.item.title,
-         subtitle: (c) => c.item.subtitle,
-         href: (c) => c.item.href,
-         target: (c) => c.item.target,
-      },
-   };
+  export default {
+    props: { item: { type: Object } },
+    computed: {
+      title: (c) => c.item.title,
+      subtitle: (c) => c.item.subtitle,
+      href: (c) => c.item.href,
+      target: (c) => c.item.target,
+    },
+  };
 </script>
 
 <template>
-   <a class="ContactGroupItem" :href="href" :target="target">
-      <span>{{ title }}</span>
-      <span>{{ subtitle }}</span>
-   </a>
+  <a class="ContactGroupItem" :href="href" :target="target">
+    <span>{{ title }}</span>
+    <span>{{ subtitle }}</span>
+  </a>
 </template>
 
 <style lang="scss" scoped>
-   .ContactGroupItem {
-      display: flex;
-      flex-direction: row;
-      flex-wrap: wrap;
-      align-items: center;
-      justify-content: flex-start;
+  .ContactGroupItem {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    align-items: center;
+    justify-content: flex-start;
 
-      text-decoration: none;
-      color: black;
-      border-radius: 0.5em;
-      font-size: 0.8em;
-      column-gap: 0.5em;
+    text-decoration: none;
+    color: black;
+    border-radius: 0.8em;
+    font-size: 0.8em;
+    column-gap: 0.5em;
+    padding: 1.2em;
 
-      &:hover {
-         text-decoration: underline;
-      }
-   }
+    border: 1px solid hsla(0, 0%, 0%, 0.1);
+    transition: all 200ms linear;
+    background: white;
+
+    &:hover {
+      text-decoration: underline;
+      background: hsla(0, 0%, 0%, 0.05);
+    }
+  }
 </style>
--- src/pages/home/PageHome-SectionContact-Group.vue
+++ src/pages/home/PageHome-SectionContact-Group.vue
@@ -1,84 +1,85 @@
 <script>
-   import Item from "./PageHome-SectionContact-Group-Item.vue";
+  import Contact from "./PageHome-SectionContact-Group-Item.vue";
 
-   export default {
-      components: { Item },
-      props: {
-         isThin: { type: Boolean, default: false },
-         group: { type: Object },
-      },
-      computed: {
-         title: (c) => c.group.category.title,
-         icon: (c) => c.group.category.icon,
-         items: (c) => c.group.items,
-
-         firstItem: (c) => (c.items.length > 0 ? c.items[0] : null),
-         fisrtHref: (c) => c.firstItem?.href ?? "",
-         fisrtTarget: (c) => c.firstItem?.target ?? undefined,
-      },
-   };
+  export default {
+    components: { Contact },
+    props: {
+      isThin: { type: Boolean, default: false },
+      group: { type: Object },
+    },
+    computed: {
+      title: (c) => c.group.category.title,
+      icon: (c) => c.group.category.icon,
+      items: (c) => c.group.items,
+    },
+  };
 </script>
 
 <template>
-   <a
-      :class="['ContactGroup', 'transition']"
-      :isThin="`${isThin}`"
-      :href="fisrtHref"
-      :target="fisrtTarget"
-   >
-      <img class="ContactGroup-icon" :src="icon" />
-      <div class="ContactGroup-main">
-         <span class="ContactGroup-title">{{ title }}</span>
-         <Item v-for="item of items" :key="item.title" :item="item" />
-      </div>
-   </a>
+  <div :class="['ContactGroup', 'transition']" :isThin="`${isThin}`">
+    <div class="ContactGroup-icon" :style="{ 'grid-area': 'icon' }">
+      <img :src="icon" :alt="title" />
+    </div>
+    <span class="ContactGroup-title" :style="{ 'grid-area': 'title' }">{{
+      title
+    }}</span>
+    <div class="ContactGroup-main" :style="{ 'grid-area': 'main' }">
+      <Contact v-for="item of items" :key="item.title" :item="item" />
+    </div>
+  </div>
 </template>
 
 <style lang="scss" scoped>
-   .ContactGroup {
-      overflow: hidden;
-      color: black;
-      text-decoration: none;
-      position: relative;
+  .ContactGroup {
+    --icon-size: 2.2em;
 
-      width: 100%;
+    overflow: hidden;
+    color: black;
+    text-decoration: none;
+    position: relative;
+
+    width: 100%;
+    height: 100%;
+    padding: 1em;
+    border-radius: 1em;
+    gap: 1em;
+
+    display: grid;
+    grid-template-areas:
+      "icon title"
+      "main main";
+    grid-template-columns: var(--icon-size) 1fr;
+    grid-template-rows: 1fr;
+
+    .ContactGroup-icon {
+      width: var(--icon-size);
+      height: var(--icon-size);
+
+      padding: 0.2em;
+      background: white;
+      border-radius: 50%;
+
+      display: grid;
+      place-items: center;
+
+      img {
+        width: 60%;
+        height: 60%;
+      }
+    }
+    .ContactGroup-title {
+      font-size: 1.2em;
+      align-self: center;
+    }
+    .ContactGroup-main {
+      flex-grow: 1;
       height: 100%;
-      padding: 1em;
-      border-radius: 1em;
-      gap: 1em;
+      gap: 0.2em;
 
       display: flex;
       flex-direction: row;
+      flex-wrap: wrap;
       align-items: flex-start;
-      justify-content: center;
-
-      &:hover,
-      &:focus {
-         background: rgba(0, 0, 0, 0.1);
-      }
-
-      .ContactGroup-main {
-         flex-grow: 1;
-         height: 100%;
-         gap: 0.5em;
-
-         display: flex;
-         flex-direction: column;
-         align-items: flex-start;
-
-         .ContactGroup-title {
-            font-weight: 600;
-         }
-      }
-      .ContactGroup-icon { 
-         --size: 5em;
-         width: var(--size);
-         height: var(--size);
-
-         padding: 1.5em;
-
-         background: white;
-         border-radius: 1.2em;
-      }
-   }
+    }
+  }
 </style>
