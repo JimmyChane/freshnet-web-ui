3a8256d6ff95fbd8cdef84be3dc287b555f3b5f7
JimmyChane <63626658+JimmyChane@users.noreply.github.com>
2023-08-16 20:45:39 +0800

    Improve/Integrated to vue-pwa

--- vue.config.js
+++ vue.config.js
@@ -28,7 +28,7 @@ const AppendIdentifierPlugin = {
   },
 };
 
-const vueConfig = {
+module.exports = defineConfig({
   transpileDependencies: true,
   devServer: { port: freshnetConfig.devPort },
   configureWebpack: {
@@ -58,6 +58,67 @@ const vueConfig = {
       return args;
     });
   },
-};
 
-module.exports = defineConfig(vueConfig);
+  pwa: {
+    name: "Freshnet Enterprise",
+    themeColor: "#2f579d",
+    msTileColor: "#f0f0f0",
+    appleMobileWebAppCapable: "yes",
+    appleMobileWebAppStatusBarStyle: "black",
+
+    iconPaths: {
+      favicon16: "./icon/favicons-16.ico",
+      favicon32: "./icon/favicons-32.ico",
+      faviconSVG: "./icon/favicon.svg",
+    },
+
+    manifestOptions: {
+      name: "Freshnet Enterprise",
+      short_name: "Freshnet",
+      icons: [
+        { src: "./icon/logos-129.png", type: "image/png", sizes: "129x129" },
+        { src: "./icon/logos-192.png", type: "image/png", sizes: "192x192" },
+        { src: "./icon/logos-512.png", type: "image/png", sizes: "512x512" },
+        {
+          src: "./icon/logos-512.svg",
+          type: "image/svg+xml",
+          sizes: "512x512",
+        },
+      ],
+      start_url: "/",
+      scope: "./",
+      display: "standalone",
+      background_color: "#f0f0f0",
+      theme_color: "#2f579d",
+    },
+
+    workboxOptions: {
+      runtimeCaching: [
+        {
+          urlPattern: /^https:\/\/api\.freshnet\.app\//,
+          method: "GET",
+          handler: "NetworkFirst",
+          options: {
+            cacheName: "api.freshnet.app-precache",
+          },
+        },
+        {
+          urlPattern: /^https:\/\/res\.freshnet\.app\//,
+          method: "GET",
+          handler: "StaleWhileRevalidate",
+          options: {
+            cacheName: "res.freshnet.app-precache",
+          },
+        },
+        {
+          urlPattern: /^(?!https:\/\/(?:res|api)\.freshnet\.app\/).*/,
+          method: "GET",
+          handler: "NetworkFirst",
+          options: {
+            cacheName: "external-precache",
+          },
+        },
+      ],
+    },
+  },
+});
